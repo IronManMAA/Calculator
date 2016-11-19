eec6c75a6506d1f9b628c76fa4e835449da5e590
Marco Almeida <marcoalmeida@Marcos-MacBook-Pro.local>
2016-11-19 12:30:01 -0500

    better version

--- Calculator/CalcViewController.m
+++ Calculator/CalcViewController.m
@@ -9,7 +9,8 @@
 #import "CalcViewController.h"
 
 
-@interface CalcViewController () <NumSetDelegate>
+@interface CalcViewController ()
+// <NumSetDelegate>
 // <NumSetDelegate> is the signature that this TimerViewController.m file conforms to the protocal set in the SetTimerViewController.h file header id
 
 @property (weak, nonatomic) IBOutlet UILabel *DisplayFrame;
@@ -21,8 +22,10 @@
 @property (assign) NSString *DisplayText;
 @property (assign) NSString *NumValST;
 @property (assign) NSString *operandAction;
-@property (assign) BOOL decimalDot;
 @property (assign) BOOL done;
+
+//@property (assign) NSString *newvarEntry;
+
 @end
 
 @implementation CalcViewController
@@ -30,15 +33,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-
     self.DisplayFrame.text=@"";
     self.operandAction=nil;
     self.numVal1 = 0;
     self.numVal2 = 0;
     self.numTotal = 0;
-    self.decimalDot=NO;
     self.done=NO;
-
 }
 
 
@@ -51,16 +51,31 @@
 #pragma mark - Action  Handlers
 
 
-- (IBAction)B0:(UIButton *)sender {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"0"];}
-- (IBAction)B1:(UIButton *)sender {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"1"];}
-- (IBAction)B2:(UIButton *)sender {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"2"];}
-- (IBAction)B3:(UIButton *)sender {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"3"];}
-- (IBAction)B4:(UIButton *)sender {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"4"];}
-- (IBAction)B5:(UIButton *)sender {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"5"];}
-- (IBAction)B6:(UIButton *)sender {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"6"];}
-- (IBAction)B7:(UIButton *)sender {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"7"];}
-- (IBAction)B8:(UIButton *)sender {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"8"];}
-- (IBAction)B9:(UIButton *)sender {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"9"];}
+- (IBAction)B0:(UIButton *)sender {
+    if (self.done) {
+        self.DisplayFrame.text=@"";
+        self.done=NO;
+        }
+    {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:sender.titleLabel.text];}
+
+}
+
+- (IBAction)BOperand:(UIButton *)sender {
+    if (!self.operandAction) {
+    {self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:sender.titleLabel.text];}
+    self.operandAction=sender.titleLabel.text;
+    }
+    else
+    {
+        self.DisplayFrame.text=@"";
+        self.operandAction=nil;
+        self.numVal1 = 0;
+        self.numVal2 = 0;
+        self.numTotal = 0;    
+        self.done=NO;
+        // replace Operand from DisplayFrame and reassign operandAction variable
+    }
+    }
 
 
 - (IBAction)BClear:(UIButton *)sender
@@ -70,39 +85,8 @@
     self.numVal1 = 0;
     self.numVal2 = 0;
     self.numTotal = 0;
-    self.decimalDot=NO;
-    self.done=YES;
- 
+    self.done=NO;
 }
-
- - (IBAction)BMult:(UIButton *)sender
- {
-    self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"*"];
-    self.operandAction=@"*";
- }
- - (IBAction)BMinus:(UIButton *)sender
- {
-    self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"-"];
-    self.operandAction=@"-";
- }
- - (IBAction)BDiv:(UIButton *)sender
- {
-    self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"/"];
-    self.operandAction=@"/";
- }
- - (IBAction)BPlus:(UIButton *)sender
-  {
-       self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"+"];
-       self.operandAction=@"+";
-  }
- - (IBAction)BDot:(UIButton *)sender
- {
-    if (!self.decimalDot) {
-        self.decimalDot=YES;
-        self.DisplayFrame.text=[self.DisplayFrame.text stringByAppendingString:@"."];
-    }
- }
-
 
 
 - (IBAction)BEqual:(UIButton *)sender
@@ -146,9 +130,7 @@
       self.numVal1 = 0;
       self.numVal2 = 0;
       self.numTotal = 0;
-      self.decimalDot=NO;
       self.done=YES;
-
 
     }
 
